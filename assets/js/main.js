465fc413f248b426b4445eada47c2cb83d1a54b3
Marlon Pilco (Bedimcode) <bedimcode@gmail.com>
2023-05-13 08:59:28 -0500

    Added source code

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,28 @@
 /*=============== SHOW HIDDEN - PASSWORD ===============*/
+const showHiddenPass = (inputPass, inputIcon) =>{
+   const input = document.getElementById(inputPass),
+         iconEye = document.getElementById(inputIcon)
+         
+   iconEye.addEventListener('click', () =>{
+       // Change password to text
+       if(input.type === 'password'){
+           // Switch to text
+           input.type = 'text'
+
+           // Add icon
+           iconEye.classList.add('ri-eye-line')
+           // Remove icon
+           iconEye.classList.remove('ri-eye-off-line')
+       }else{
+           // Change to password
+           input.type = 'password'
+
+           // Remove icon
+           iconEye.classList.remove('ri-eye-line')
+           // Add icon
+           iconEye.classList.add('ri-eye-off-line')
+       }
+   })
+}
+
+showHiddenPass('input-pass','input-icon')
